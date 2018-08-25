9e4c3fe7bccb90b00221d18b4b6efa21b0e03104
nimbus117 <mail@nimbus117.co.uk>
2018-08-25 14:18:52 +0100

    fixed PG.connect, needed brackets

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -3,7 +3,7 @@ require 'pg'
 class SqlRunner
   def SqlRunner.run sql, values = [] 
     begin
-      db = PG.connect {dbname: 'inventory', host: 'localhost'} 
+      db = PG.connect({dbname: 'inventory', host: 'localhost'})
       db.prepare('query', sql)
       result = db.exec_prepared 'query', values 
     ensure
