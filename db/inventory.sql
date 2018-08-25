630dcab743f298393354c52dd4ea60a48c7b92b8
nimbus117 <mail@nimbus117.co.uk>
2018-08-25 14:04:23 +0100

    added sql script and SqlRunner

---
+++ db/inventory.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS inventory_items;
+DROP TABLE IF EXISTS manufacturers;
+
+CREATE TABLE manufacturers(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL,
+  notes TEXT
+);
+
+CREATE TABLE inventory_items(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  manufacturer_id INT8 REFERENCES manufacturers(id),
+  cost_price INT2 NOT NULL,
+  sell_price INT2 NOT NULL,
+  quantity INT2 NOT NULL,
+  notes TEXT
+);
