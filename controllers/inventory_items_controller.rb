79a88b28971b916bfa293307d71db7a0ecd7b9d7
nimbus117 <mail@nimbus117.co.uk>
2018-08-27 09:53:51 +0100

    added show view for individual items

--- controllers/inventory_items_controller.rb
+++ controllers/inventory_items_controller.rb
@@ -5,3 +5,8 @@ get '/inventory' do
   @inventory = InventoryItem.all
   erb :"inventory_items/index"
 end
+
+get '/inventory/:id' do
+  @item = InventoryItem.get_by_id params[:id].to_i
+  erb :"inventory_items/show"
+end
