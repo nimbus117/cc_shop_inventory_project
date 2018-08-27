935b6acefa497a147537fc766f8b522f64df7756
nimbus117 <mail@nimbus117.co.uk>
2018-08-27 10:34:47 +0100

    added show view/route for individual manufacturers

--- controllers/manufacturers_controller.rb
+++ controllers/manufacturers_controller.rb
@@ -5,3 +5,8 @@ get '/manufacturers' do
   @manufacturers = Manufacturer.all
   erb :"manufacturers/index"
 end
+
+get '/manufacturers/:id' do
+  @manufacturer = Manufacturer.get_by_id params[:id].to_i
+  erb :"manufacturers/show"
+end
