0f33e4c6bac8aa0b4c3503ff68a0ca9efec63fe4
nimbus117 <mail@nimbus117.co.uk>
2018-08-27 14:13:38 +0100

    added new view/route for manufacturer

--- controllers/manufacturers_controller.rb
+++ controllers/manufacturers_controller.rb
@@ -6,6 +6,16 @@ get '/manufacturers' do
   erb :"manufacturers/index"
 end
 
+get '/manufacturers/new' do
+  erb :"manufacturers/new"
+end
+
+post '/manufacturers/new' do
+  new_manufacturer = Manufacturer.new(params)
+  new_manufacturer.save
+  redirect to "/manufacturers/#{new_manufacturer.id}"
+end
+
 get '/manufacturers/:id' do
   @manufacturer = Manufacturer.get_by_id params[:id].to_i
   erb :"manufacturers/show"
